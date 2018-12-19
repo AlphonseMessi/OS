8170fdec20429b116b28248efe1e12875a6152ac
AlphonseMessi <1450658705@qq.com>
2018-12-19 10:34:52 +0800

    Update sender.c

--- shiyan3/4/sender.c
+++ shiyan3/4/sender.c
@@ -20,8 +20,8 @@ int main()
     	int shmid = shmget(key, sizeof(message), 0666 | IPC_CREAT);
 	check_error(shmid);
     	void* shmp = shmat(shmid, NULL, 0);
-    	sem_send = sem_open(name, O_CREAT, 0666, 1);
-	sem_recive = sem_open(name, O_CREAT, 0666, 1);
+    	sem_send = sem_open(name1, O_CREAT, 0666, 1);
+	sem_recive = sem_open(name2, O_CREAT, 0666, 1);
 	int i;
 	if(sem_getvalue(sem_send,&i)==0){
 		while(i>2)
@@ -49,8 +49,8 @@ int main()
 	sem_post(sem_send);
     }
 
-	sem_unlink(name);
-
+	sem_unlink(name1);
+        sem_unlink(name2);
     	shmctl(shmid, IPC_RMID, shmp);
     	printf("sender end\n");
     	return 0;
